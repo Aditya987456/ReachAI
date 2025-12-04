8217525d51b9a8c2e1909ccf0bb546edc4933f4f
Aditya raj <adityaraj987456@gmail.com>
2025-12-05 00:39:16 +0530

    Hero section, form, navigation, footer section is completed full production ready

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,97 +1,109 @@
 
+
+
 import HeroForm from "./heroform";
+
+
+
+
 export default function Hero() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   return (
     <section
-      className="w-full py-28 md:py-40"
+      className="w-full py-20 sm:py-28 lg:py-32 xl:py-36"
       style={{
-        background: "linear-gradient(97.79deg, #FFFAFA 28.74%, #FF0000 117.88%)",
+        background:
+          "linear-gradient(97.79deg, #FFFAFA 28.74%, #FF0000 117.88%)",
       }}
     >
-      <div className="max-w-[1440px] mx-auto px-6 lg:px-40 grid grid-cols-1 lg:grid-cols-12 gap-10 items-center">
+      <div className="max-w-[1440px] mx-auto px-6 xl:px-28 grid grid-cols-1 xl:grid-cols-12 gap-12 items-center">
 
-        {/* LEFT side */}
-        <div className="lg:col-span-7 space-y-7 text-center lg:text-left">
-         
-         {/* <span className="px-4 py-1 border border-red-600 bg-red-100 text-red-600 rounded-full text-sm font-semibold">
-          <span className="inline-block w-2 h-2  bg-red-600 rounded-full mr-1 shadow-[0_0_4px_rgba(255,0,0,0.3)] animate-pulse"></span>
-          AI optimized
-        </span> */}
+        {/* ------- Left side ----- */}
+        <div
+          className="
+          xl:col-span-7 
+          space-y-7 
+          text-center xl:text-left
+          
+          /* --- LG ONLY CHANGES --- */
+          md:max-w-3xl
+          lg:max-w-4xl 
+          md:mx-auto
+          lg:space-y-9
+          "
+        >
+          {/* AI optimized - badge*/}
+          <span className="px-4 py-1 border border-red-600 bg-red-100 text-red-600 rounded-full text-sm font-semibold inline-flex items-center gap-2 mx-auto xl:mx-0">
+            <span className="relative flex h-2 w-2">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-2 w-2 bg-red-600"></span>
+            </span>
+            AI optimized
+          </span>
 
+          {/* Heading */}
+          <h1 className="
+            text-3xl sm:text-4xl md:text-5xl 
+            font-extrabold leading-snug xl:leading-tight 
+            max-w-2xl mx-auto xl:mx-0
 
-<span className="px-4 py-1 border border-red-600 bg-red-100 text-red-600 rounded-full text-sm font-semibold inline-flex items-center gap-2">
-  <span className="relative flex h-2 w-2">
-    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-    <span className="relative inline-flex rounded-full h-2 w-2 bg-red-600"></span>
-  </span>
-  AI optimized
-</span>
-
-
-
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold leading-snug lg:leading-tight">
+            /* lg+ screen width increase. */
+            lg:max-w-4xl
+          ">
             Boost YouTube Reach with{" "}
             <span className="text-red-600">AI-Powered</span>,{" "}
-
-
-           <span className="relative inline-block text-red-600 pr-2 pb-1">
-  Trend-Aware
-  <svg
-    className="
-      absolute left-0 bottom-0 w-full
-      h-[6px]         
-      sm:h-[7px]       
-      md:h-[8px]       
-      lg:h-[9px]       
-      xl:h-[10px]     
-    "
-    viewBox="0 0 280 10"
-    fill="none"
-    preserveAspectRatio="none"
-  >
-    <path
-      d="M4 6C70 0 210 0 276 6"
-      stroke="#DC2626"
-      strokeWidth="3"
-      strokeLinecap="round"
-    />
-  </svg>
-</span>
-
-
-
-
             
+            <span className="relative inline-block text-red-600 pr-2 pb-1">
+              Trend-Aware
+              <svg
+                className="
+                  absolute left-0 bottom-0 w-full
+                  h-[6px] sm:h-[7px] md:h-[8px] lg:h-[9px] xl:h-[10px]
+                "
+                viewBox="0 0 280 10"
+                fill="none"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M4 6C70 0 210 0 276 6"
+                  stroke="#DC2626"
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </span>
+
             Metadata.
           </h1>
 
-          <p className="text-gray-700 max-w-lg mx-auto lg:mx-0 text-sm sm:text-base md:text-lg">
+          <p className="
+            text-gray-700 max-w-lg mx-auto xl:mx-0 
+            text-sm sm:text-base md:text-lg md:max-w-xl
+
+            /* LG wider line width */
+            lg:max-w-3xl
+          ">
             ReachAI analyzes your channel, finds what's trending in your niche,
-            and generates SEO-optimized metadata that boosts your YouTube growth.
+            and generates SEO-optimized <b>titles, description, tags and hashtags</b> that boosts your YouTube growth.
           </p>
-      </div>
+        </div>
 
-        {/*-------- form generate titles right side */}
-        <HeroForm/>
+        {/* right side form */}
+        <div
+          className="
+          xl:col-span-5 
+          flex justify-center xl:justify-end w-full 
 
+          /* lg+ screen width increase. */
+          max-w-md sm:max-w-lg md:max-w-2xl 
+          lg:max-w-3xl mx-auto
+          "
+        >
+          <HeroForm />
+        </div>
       </div>
     </section>
   );
 }
+
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const path=usePathname()
 
   return (
     <header className="w-full bg-white sticky top-0 z-50 border-b border-gray-100 py-2 sm:py-3 md:py-3 lg:py-5">
@@ -20,9 +22,24 @@ export default function Navbar() {
         <nav className="hidden lg:flex items-center gap-8 text-base font-medium">
           <Link href="#how" className="hover:text-red-600 transition">How it works</Link>
           <Link href="#faq" className="hover:text-red-600 transition">FAQ</Link>
-          <Link href="about" className="hover:text-red-600 transition">About</Link>
-          <Link href="contact" className="hover:text-red-600 transition">Contact</Link>
+
+
+          <Link href="about" 
+          className={path === "/about" ? "text-red-600 transition" :"hover:text-red-600 transition"}>
+          About</Link>
+
+
+          <Link href="contact" 
+            className={path === "/contact" ? "text-red-600 transition" :"hover:text-red-600 transition"}>
+            Contact</Link>
+
         </nav>
+
+
+
+
+
+
 
         {/* Mobile Menu Button */}
         <button
@@ -40,18 +57,30 @@ export default function Navbar() {
         }`}
       >
         <nav className="flex flex-col gap-4 bg-white px-6 py-5 text-base font-medium border-t border-gray-100">
+          
+
           <Link href="#how" onClick={() => setOpen(false)} className="hover:text-red-600 transition">
             How it works
           </Link>
-          <Link href="#faq" onClick={() => setOpen(false)} className="hover:text-red-600 transition">
+
+
+          <Link href="#faq" onClick={() => setOpen(false)} 
+          className="hover:text-red-600 transition">
             FAQ
           </Link>
-          <Link href="#about" onClick={() => setOpen(false)} className="hover:text-red-600 transition">
+
+
+          <Link href="about" onClick={() => setOpen(false)} 
+          className={path === "/about" ? "text-red-600 transition" :"hover:text-red-600 transition"}>
             About
           </Link>
-          <Link href="#contact" onClick={() => setOpen(false)} className="hover:text-red-600 transition">
+
+
+          <Link href="contact" onClick={() => setOpen(false)} 
+          className={path === "/contact" ? "text-red-600 transition" :"hover:text-red-600 transition"}>
             Contact
           </Link>
+
         </nav>
       </div>
     </header>
